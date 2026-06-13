edffb05af7f0416be6386e549f20755802a65263
Kris Lee <kdlee0401@gmail.com>
2026-06-12 22:26:22 -0500

    data types chapter practice questions

---
+++ week1/data-types.js
@@ -0,0 +1,8 @@
+// Kris Lee 6/12/2026
+"use strict";
+// Guess the output of each script.
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // output : "hello 1"
+console.log( `hello ${"name"}` ); // output : "hello name"
+console.log( `hello ${name}` ); // output : "hello Ilya"
