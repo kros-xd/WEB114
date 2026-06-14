73f1a18ce5a892cfb2b8a929bbab4f4ae332e1f9
Kris Lee <kdlee0401@gmail.com>
2026-06-13 19:38:24 -0500

    Added some comments

--- week1-practice/fortune.js
+++ week1-practice/fortune.js
@@ -6,4 +6,5 @@ let location = "Seattle"; // string
 let pets = 2; // number (integer)
 let salary = "$120,000"; // string
 
+// Use template literals
 console.log(`My name is ${name} and I will be a ${career} and living in ${location}. I will have ${pets} pets and hopefully make ${salary}.`);
