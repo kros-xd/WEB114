07ab9573b126d668eb1c6807bf31cdcb5039599f
Kris Lee <kdlee0401@gmail.com>
2026-06-16 20:21:00 -0500

    Week 2 project assignment

---
+++ week2/character.js
@@ -0,0 +1,8 @@
+// Kris Lee 6/16/2026
+"use strict";
+
+// Introduction message
+alert("Welcome Kris, to the floating sky city!");
+
+// Grab user's character name
+const charName = prompt("What will you name your character?", "Wanderer");
